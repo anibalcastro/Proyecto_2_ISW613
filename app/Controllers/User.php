f94927bb6ca3f39fc0848a814062d116e906c5fe
anibalcastro <anibalcastro1515@gmail.com>
2022-04-11 01:03:35 -0600

    link action

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -28,6 +28,7 @@ class User extends BaseController{
 
     public function validateLogin(){
 
+       //https://blog.trescomatres.com/2021/04/codeigniter-4-tratar-parametros-post/
        $email = $this->request->getPost('emailU');
        $password = $this->request->getPost('passwordU');
 
